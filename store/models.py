0ebaec44682a91e1b8e1168f7807acfb0fb14efa
AkshayKumarC132 <101247420+AkshayKumarC132@users.noreply.github.com>
2022-09-08 15:11:54 +0530

    Update models.py

--- store/models.py
+++ store/models.py
@@ -2,7 +2,7 @@ import datetime
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-
+# Extending User Model as User_Profile and Some more fields 
 class User_profile(AbstractUser):
     id = models.AutoField(primary_key=True,db_column='user_id')  
     username = models.CharField(max_length=100,unique=True) 
@@ -18,6 +18,7 @@ class User_profile(AbstractUser):
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
 
+    # str function in a django model returns a string that is exactly rendered as the display name of instances for that model.
     def __str__(self):
         self.username
 
